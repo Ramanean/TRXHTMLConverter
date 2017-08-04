d271107cbe7383dd9f64efe07cdcf443df1ee57d
sxs93 <shanmuga.subramanian@lennoxintl.com>
2017-08-04 10:58:40 +0530

    changes

--- TrxerConsole/functions.js
+++ TrxerConsole/functions.js
@@ -259,7 +259,7 @@ function CalculateTestsStatuses(testContaineId, canvasId) {
     var passedPrec = (totalPassed / totalTests) * 100;
     var failedPrec = (totalFailed / totalTests) * 100;
     var warnPrec = (totalInconclusive / totalTests) * 100;
-
+    //Shan
 
     CreateHorizontalBars(canvasId, passedPrec, failedPrec, warnPrec);
 }
